35ebafbceb430eb91d6cdca21345d86dea421a9f
vanam-chandra-shekar <vanamchandrashekar@outlook.com>
2023-01-14 13:03:30 +0530

    intial commit

--- init.lua
+++
@@ -1,8 +0,0 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colour"
-require "user.cmp"
-require "user.lsp" 
-require "user.treesitter"
-require "user.tree"
--- lua/user/colour.lua
+++
@@ -1,9 +0,0 @@
-local colorscheme = "tokyonight-night"
-
-local status_ok,_= pcall(vim.cmd,"colorscheme "..colorscheme)
-
-if not status_ok then
-    vim.notify("colourscheme "..colorscheme.." not found")
-    vim.cmd "colorscheme slate"
-    return
-end
